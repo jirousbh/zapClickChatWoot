1409b91498b5924ca9826317e9bbc7553d7cf9fb
Shivam Mishra <scm.mymail@gmail.com>
2024-04-25 18:20:30 +0530

    feat: add composables for route & store

---
+++ app/javascript/dashboard/composables/store.js
@@ -0,0 +1,18 @@
+import { computed } from 'vue';
+import { getCurrentInstance } from 'vue';
+
+export const useStore = () => {
+  const vm = getCurrentInstance();
+  if (!vm) throw new Error('must be called in setup');
+  return vm.proxy.$store;
+};
+
+export const useStoreGetters = () => {
+  const store = useStore();
+  return Object.fromEntries(
+    Object.keys(store.getters).map(getter => [
+      getter,
+      computed(() => store.getters[getter]),
+    ])
+  );
+};
